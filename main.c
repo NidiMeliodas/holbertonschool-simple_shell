387b318a591d53861d1a058e479b294563527e49
NidiMeliodas <nidigashan@gmail.com>
2023-12-20 19:02:20 -0800

    atempt to fix the code

--- main.c
+++ main.c
@@ -1,51 +1,69 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define MAX_COMMAND_LENGTH 100
 
-/**
- * main - Fonction principale du programme shell basique
- *
- * Ce programme est un shell simple qui attend continuellement une entrée
- * utilisateur, l'exécute comme une commande système tant que l'utilisateur
- * ne tape pas "exit", et affiche un prompt de commande.
- *
- * Return: 0 en cas de succès, valeur d'échec autrement
- */
+void prompt() {
+	printf("$ ");
+	fflush(stdout);
+}
 
-int main(void)
-{
+void execute_cmd(char *command) {
+	pid_t pid = fork();
+
+	if (pid == -1) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	} else if (pid == 0) {
+		/* Child process */
+		execlp(command, command, (char *)NULL);
+		perror("exec");
+		exit(EXIT_FAILURE);
+	} else {
+		/* Parent process */
+		waitpid(pid, NULL, 0);
+	}
+}
+
+int main() {
 	char *command = NULL;
 	size_t command_size = 0;
 	size_t command_length;
 
-	while (1)
-	{
-		prompt();
-		/* lis la command que l'utilisateur tape avec un getline */
-		if (getline(&command, &command_size, stdin) == -1)
-		{
-			printf("\n");
-			free(command);
-			break;
+		while (1) {
+			prompt();
+
+			/* Read the command from the user using getline */
+			if (getline(&command, &command_size, stdin) == -1) {
+				/* Handle end-of-file (Ctrl+D) */
+				printf("\n");
+				free(command);
+				break;
+			}
+
+			/* Remove the newline character at the end */
+			command_length = strlen(command);
+			if (command_length > 0 && command[command_length - 1] == '\n') {
+				command[command_length - 1] = '\0';
+			}
+
+			/* Check for "exit" command */
+			if (strcmp(command, "exit") == 0) {
+				printf("Exiting the shell.\n");
+				free(command);
+				break;
+			}
+
+			/* Execute the command */
+			execute_cmd(command);
 		}
-		/* Supprimez le caractère de saut de ligne à la fin */
-		command_length = strlen(command);
-		if (command_length > 0 && command[command_length - 1] == '\n')
-		{
-			command[command_length - 1] = '\0';
-		}
-		/* Verifi si la commande exit est la */
-		if (strcmp(command, "exit") == 0)
-		{
-			printf("Exiting the shell.\n");
-			free(command);
-			break;
-		}
-		/* Execute la commande */
-		execute_cmd(command);
-	}
-	/* Libére la mémoire dynamique allouée */
+
+	/* Free the dynamically allocated memory */
 	free(command);
 
-	return (0);
+	return 0;
 }
+
